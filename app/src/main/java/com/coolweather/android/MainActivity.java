779eafe2ae60c04d16bdfbf8620df378e29caf8b
liang <liang.com>
2019-12-18 13:32:15 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SQLiteDatabaseHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper = new SQLiteDatabaseHelper(this, "cool_weather.db", null, 1);
+        dbHelper.getWritableDatabase();
     }
 }
---
+++ app/src/main/java/com/coolweather/android/SQLiteDatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.coolweather.android;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class SQLiteDatabaseHelper extends SQLiteOpenHelper {
+    public static final String CREATE_PROVINCE = "create table Province("
+            + "id integer primary key autoincrement,"
+            + "provinceName text,"
+            + "provinceCode integer)";
+    public static final String CREATE_CITY = "create table City("
+            + "id integer primary key autoincrement,"
+            + "cityName text)";
+    public static final String CREATE_COUNTY = "create table County("
+            + "id integer primary key autoincrement,"
+            + "countyName text,"
+            + "weatherId text,"
+            + "cityId integer)";
+    private Context mContext;
+
+    public SQLiteDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+        Log.d("a","a");
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Province");
+        db.execSQL("drop table if exists City");
+        db.execSQL("drop table if exists Country");
+        onCreate(db);
+        Log.d("b","b");
+    }
+}
